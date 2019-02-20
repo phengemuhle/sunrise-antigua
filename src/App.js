8fd9e120b9d0120a802af07b3b8d9ac96a4b5ff0
phengemuhle <phengemuhle@gmail.com>
2019-02-19 16:15:00 -1000

    built react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route } from 'react-router-dom'
+import Header from "./componants/Header"
+import HomePage from "./componants/HomePage"
+
+
 
 class App extends Component {
+  constructor(props){
+    super();
+    this.state ={
+
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header/>
+        <div>
+          <Route path="/" exact render={() => <HomePage/>}/>
+        </div>
       </div>
     );
   }
---
+++ src/componants/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Header = (props) => {
+    return (
+    <div> 
+        <h1>this is the title</h1>
+    </div>
+    )
+
+}
+
+export default Header
